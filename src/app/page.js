c0e44afe4174e09896585f57caa8d769ff74da45
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-01-09 11:03:51 -0300

    feat: rama prueba

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,79 @@
+"use client";
+import { useState } from "react";
+import { DragDropContext, Draggable } from "react-beautiful-dnd";
+import { StrictModeDroppable as Droppable } from "@/helpers/reactStrictModeDrag";
 
-export default function Home() {
+const initialTask = [
+  { task: "ordenar", id: 1 },
+  { task: "limpiar", id: 2 },
+  { task: "comprar groceries", id: 3 },
+  { task: "hacer ejercicio", id: 4 },
+  { task: "estudiar", id: 5 },
+  { task: "preparar cena", id: 6 },
+];
+
+const App = () => {
+  const [tasks, setTasks] = useState(initialTask);
+  console.log(tasks);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
   return (
-  
-    
-    <>
-      
+    <DragDropContext
+      onDragEnd={(result) => {
+        const { source, destination } = result;
+        if (!destination) {
+          return;
+        }
+        if (
+          source.index === destination.index &&
+          source.droppableId === destination.droppableId
+        ) {
+          return;
+        }
+        setTasks((prevTasks) => {
+          reorder(prevTasks, source.index, destination.index);
+        });
+      }}
+    >
+      <div className="app">
+        <h1>title</h1>
+        <Droppable droppableId="task1">
+          {(droppableProvider) => (
+            <ul
+              {...droppableProvider.droppableProps}
+              ref={droppableProvider.innerRef}
+            >
+              {tasks.map((task, index) => (
+                <Draggable
+                  index={index}
+                  key={index}
+                  draggableId={task.id.toLocaleString()}
+                >
+                  {(provided) => (
+                    <li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      key={task.id}
+                    >
+                      {task.task}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {droppableProvider.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+};
 
-      <h2>app</h2>
-    </>
-  )
-}
+export default App;
