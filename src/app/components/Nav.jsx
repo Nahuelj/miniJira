ed365d3e72f383b22b2fe63acfba3a5115cb498b
nicolas gonzalez <gonzaleznicolas8000@gmail.com>
2024-01-11 15:49:12 -0300

    asdsa

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -5,14 +5,18 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
 import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';import HelpIcon from "@mui/icons-material/Help";
+import EquipoIcons from "../icons/EquipoIcons";
+
 import noAvatar from "../../../public/noAvatar.png";
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import Image from "next/image";
+import HelpIcons from "../icons/HelpIcons";
 
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import styled from "styled-components";
+import SettingIcons from "../icons/SettingIcons";
+import BoardIcon from "../icons/HomeIcons";
+import NotificationIcons from "../icons/NotificationIcons";
 
 const Nav = () => {
   const Cs = styled.p`
@@ -22,7 +26,7 @@ const Nav = () => {
 
   return (
     <div className="">
-      <Sidebar backgroundColor="#F7F7F7" className=" h-screen">
+      <Sidebar  backgroundColor="#0E0E0E" className="text-white h-screen" style={{ left: 'auto', right: 0 }}>
         <div className="flex items-center mt-5 justify-center  gap-2 max-w-52 mx-auto ">
           <div>
             <Image className="w-20 h-20 rounded-full" src={noAvatar} />
@@ -30,25 +34,40 @@ const Nav = () => {
           </div>
         </div>
 
-        <Menu className="h-full ">
+        <Menu className="h-full   "   menuItemStyles={{
+           button: {
+             backgroundColor: '#0E0E0E', // Color de fondo cuando no se pasa el cursor
+             '&:hover': {
+               backgroundColor: '#F73859', 
+             },
+           },
+         }}>
+      
+
+
           <SubMenu
-            className="pt-3 pb-3"
+            className="pt-3 pb-3 "
             active={true}
-            icon={<HomeOutlinedIcon className="text-3xl w-9 text-blue-600 font-light " />}
+            
+            icon={<BoardIcon className="  text-3xl text-blue-600 w-9"/>}
             label="Tablero Principal"
           >
             <MenuItem> opcion 1 </MenuItem>
             <MenuItem> opcion 2 </MenuItem>
           </SubMenu>
-          <MenuItem className=" pt-3 pb-3 " icon={<AssignmentOutlinedIcon  className=" text-3xl w-9 text-blue-600 "/>}> Tareas </MenuItem>
-          <MenuItem className="pt-3 pb-3" icon={<NotificationsNoneOutlinedIcon className=" text-3xl text-blue-600 w-9 " />}> Notificaciones </MenuItem>
-          <MenuItem className="pt-3 pb-3" icon={<GroupOutlinedIcon className=" text-3xl text-blue-600 w-9 " />}> Equipo </MenuItem>
-          <MenuItem className="pt-3 pb-3" icon={<SettingsOutlinedIcon className="font-light  text-3xl text-blue-600 w-9" />}> Configuraciones </MenuItem>
+        
+          <MenuItem className="pt-3 pb-3" icon={<NotificationIcons className=" text-3xl text-blue-600 w-9 " />}> Notificaciones </MenuItem>
+          <MenuItem className="pt-3 pb-3" icon={<EquipoIcons className=" text-3xl text-blue-600 w-9 " />}> Equipo </MenuItem>
+          <MenuItem className="pt-3 pb-3" icon={<SettingIcons className="font-light  text-3xl  w-9" />}> Configuraciones </MenuItem>
 
-          <MenuItem className="pt-3 pb-3" active={true} icon={<ContactSupportOutlinedIcon className=" text-3xl text-blue-600 w-9"  />}>
+          <MenuItem className="pt-3 pb-3" active={true} icon={<HelpIcons className=" text-3xl text-blue-600 w-9"  />}>
             {" "}
             Ayuda{" "}
           </MenuItem>
+
+
+
+
         </Menu>
       </Sidebar>
 
