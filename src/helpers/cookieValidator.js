7e2a10a1ade0afa8b3daecacef80c339d22ac884
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-01-12 16:03:47 -0300

    feat: cookieValidator

---
+++ src/helpers/cookieValidator.js
@@ -0,0 +1,25 @@
+import { cookies } from "next/headers";
+import { verifyTokenJose, createToken } from "@/helpers/jwtHelper";
+import { res } from "@/helpers/nextResponses";
+
+export async function setCookieJwt(payload) {
+  cookies().set({
+    name: "authorization",
+    value: createToken(payload),
+    httpOnly: true,
+    path: "/",
+  });
+}
+
+export async function cookieJwtValidator(req) {
+  const authCookie = req.cookies.get("authorization")?.value;
+  if (!authCookie) {
+    return res(401);
+  }
+  try {
+    verifyTokenJose(authCookie);
+    console.log("valido");
+  } catch (error) {
+    return res(401);
+  }
+}
