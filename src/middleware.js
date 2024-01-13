1201e3a8cbc8b16db103aacd1945c513a908e3b2
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2024-01-12 23:38:27 -0300

    feat: middleware /board

--- src/middleware.js
+++ src/middleware.js
@@ -1,50 +1,17 @@
+import { authMiddleware } from "@/middlewares/auth";
+import { boardMiddleware } from "@/middlewares/board";
+import { loginAndRegisterMiddleware } from "@/middlewares/loginAndRegister";
 import { NextResponse } from "next/server";
-import { verifyTokenJose } from "@/helpers/jwtHelper";
 
 export default async function middleware(req) {
-  const authCookie = req.cookies.get("authorization")?.value;
-  if (req.nextUrl.pathname === "/") {
-    if (!authCookie) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    } else {
-      try {
-        await verifyTokenJose(authCookie);
-        return NextResponse.next();
-      } catch (error) {
-        console.log(error);
-        return NextResponse.redirect(new URL("/login", req.url));
-      }
-    }
-  }
+  const response = await authMiddleware(req);
+  if (response) return response;
 
-  if (
-    req.nextUrl.pathname === "/login" ||
-    req.nextUrl.pathname === "/register"
-  ) {
-    if (authCookie) {
-      try {
-        await verifyTokenJose(authCookie);
-        return NextResponse.redirect(new URL("/", req.url));
-      } catch (error) {
-        console.log(error);
-        return NextResponse.next();
-      }
-    }
-  }
+  const response2 = await boardMiddleware(req);
+  if (response2) return response2;
 
-  if (req.nextUrl.pathname.startsWith("/board")) {
-    if (!authCookie) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    } else {
-      try {
-        await verifyTokenJose(authCookie);
-        return NextResponse.next();
-      } catch (error) {
-        console.log(error);
-        return NextResponse.redirect(new URL("/login", req.url));
-      }
-    }
-  }
+  const response3 = await loginAndRegisterMiddleware(req);
+  if (response3) return response3;
 
   return NextResponse.next();
 }
